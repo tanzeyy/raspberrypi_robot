fbd91fb509f3e144a084f80d59eda178c11893d7
tanzeyy <tan_ze@outlook.com>
2017-04-02 19:15:57 +0800

    change to half  shelf

--- images_classify/preproc/capture.py
+++ images_classify/preproc/capture.py
@@ -2,44 +2,41 @@ import cv2
 import numpy as np
 import os
 
-upper = [0, 360]
-lower = [360, 720]
-width = 213
+upper = [0, 240]
+lower = [240, 480]
+width = 206
 w = [0, width]
 blocks = {
-    "12": (w, lower), "11": (w, upper),
-    "10": ([i+width for i in w], lower),
-    "9": ([i+width for i in w], upper),
-    "8": ([i+width*2 for i in w], lower),
-    "7": ([i+width*2 for i in w], upper),
-    "6": ([i+width*3 for i in w], lower),
-    "5": ([i+width*3 for i in w], upper),
-    "4": ([i+width*4 for i in w], lower),
-    "3": ([i+width*4 for i in w], upper),
-    "2": ([i+width*5 for i in w], lower),
-    "1": ([i+width*5 for i in w], upper)
+    "6": (w, lower), "5": (w, upper),
+    "4": ([i+width for i in w], lower),
+    "3": ([i+width for i in w], upper),
+    "2": ([i+width*2 for i in w], lower),
+    "1": ([i+width*2 for i in w], upper),
 }
 
 
-def Capture(shelf, cap_src):
+def Capture(shelf, side, cap_src):
 
     cap = cv2.VideoCapture(cap_src)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1280)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
     if not os.path.exists('images_classify/images/%s' % shelf):
         os.makedirs('images_classify/images/%s' % shelf)
 
-    for i in xrange(5):
+    for i in xrange(10):
         cap.read()
 
     ret, frame = cap.read()
     for block, area in blocks.items():
         img = frame[area[1][0]:area[1][1], area[0][0]:area[0][1]]
+        if side == "left":
+            block = str(int(block) + 6)
         cv2.imwrite(("images_classify/images/%s/%s%s" + ".jpg") %
                     (shelf, shelf, block), img)
-    # cv2.imwrite('images_classify/images/test.jpg', frame)
+    cv2.imwrite('images_classify/images/test.jpg', frame)
 
     cap.release()
     cv2.destroyAllWindows()
 
-# Capture("A", 0)
+# Capture("A", "right", 1)
+# Capture("A", "left", 0)
--- test.py
+++ test.py
@@ -7,8 +7,7 @@ import images_classify.preproc.capture as cap
 import images_classify.detectors.is_yellow as iy
 
 
-def run(shelf, cap_src=0):
-    cap.Capture(shelf, cap_src)
+def ImageClassify(shelf):
     images = os.listdir("images_classify/images/%s" % shelf)
 
     if shelf == 'A':
@@ -16,17 +15,23 @@ def run(shelf, cap_src=0):
     else:
         results = json.dumps(li.LabelImages(images, shelf))
 
-    if not os.path.exists('images_classify/results'):
-        os.makedirs('images_classify/results')
+    if not os.path.exists("images_classify/results"):
+        os.makedirs("images_classify/results")
 
     with open("images_classify/results/%s.json" % shelf, 'wr') as f:
         f.write(results)
 
+
+def run(shelf, side):
+    cap.Capture(shelf, side)
+    if side == "left":
+        ImageClassify(shelf)
+
 if __name__ == '__main__':
     import sys
     try:
-        shelf = sys.argv[1]
+        shelf, side = sys.argv[1:]
     except:
-        shelf = 'A'
+        shelf, side = 'A', 'right'
 
-    run(shelf)
+    run(shelf, side, )
