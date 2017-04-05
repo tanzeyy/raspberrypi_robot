d713d6fa44a02d5e80bedcd781bd285c217947af
unknown <>
2017-04-05 12:18:39 +0800

    tmp file for cal the path and grap things

---
+++ robot_control/grab.py
@@ -0,0 +1,8 @@
+import serial
+
+
+class Grab(object):
+
+    def __init__(self, position='upper', obj='jdb'):
+        self.position = positio
+        self.obj = obj
---
+++ robot_control/path.py
@@ -0,0 +1,11 @@
+class Path(object):
+
+    def __init__(self, loc, des):
+        self.loc = loc
+        self.des = des
+
+    def calpath(self):
+        pass
+
+    def getpath(self):
+        return calpath()
