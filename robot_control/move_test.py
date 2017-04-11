2c54231d0326a3a9b1859b329cbdb2b699c659b5
tanzeyy <tan_ze@outlook.com>
2017-04-11 14:31:27 +0800

    修改了端口和移动测试文件

--- robot_control/move_test.py
+++ robot_control/move_test.py
@@ -5,10 +5,8 @@ start = (1, 3)
 go = Move()
 init(go)
 while 1:
-    w = raw_input('Please input goal-width:')
-    h = raw_input('Please input goal-height:')
-    goal = (int(w), int(h))
-    route = search.get_route(start, goal)
-    for way in route:
-        go.run(way[0], way[1])
-    start = goal
+    for goal in [(3, 3), (2, 9), (10, 8), (3, 3)]:
+        route = search.get_route(start, goal)
+        for way in route:
+            go.run(way[0], way[1])
+        start = goal
--- sertest.py
+++ sertest.py
@@ -1,6 +1,6 @@
 import serial
 import json
-import test
+import classify
 
 port = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 
@@ -8,8 +8,9 @@ while True:
     rcv = port.read(10)
     print("Reciving:" + rcv)
     if rcv == "capture":
-        test.run()
-        with open('images_classify/results/results.json') as results:
+        shelf = "A"
+        classify.run('A', 'right')
+        with open(('images_classify/results/%s' + '.json') % shelf) as results:
             results = json.load(results)
             for position, result in results.items():
                 msg = (position + ':' + result + ';').encode('ascii')
