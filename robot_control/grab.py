828ecb6413b278ad76473bcd66ba82608bdb09cf
tanzeyy <tan_ze@outlook.com>
2017-04-11 14:35:44 +0800

    删除了一些文件

--- robot_control/grab.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-
-class Grab(object):
-
-    def __init__(self, position='upper', obj='jdb'):
-        self.position = positio
-        self.obj = obj
--- robot_control/grab_and_place.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-
-class Grab(object):
-    def __init__(self, position='upper', obj='jdb'):
-        self.position = position
-        self.obj = obj
