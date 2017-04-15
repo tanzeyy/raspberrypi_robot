7bd4632dc9de9c015d8c42124a9399551dc8b9d2
tanzeyy <tan_ze@outlook.com>
2017-04-15 16:04:12 +0800

    优化了数据存储结构

--- robot_control/dicts.py
+++ robot_control/dicts.py
@@ -1,15 +1,14 @@
-objects = {
-    "yellow cube": ['2', (5, 3), 'bD'],
-    "yakult": ['3', (7, 3), 'fD'],
-    "jdb": ['4', (8, 5), 'cD'],
-    "tennis ball": ['5', (8, 6), 'eD'],
-    "mimi": ['6', (8, 7), 'eD'],
-    "wired ball": ['7', (6, 8), 'dD'],
-    "shuttercock": ['8', (6, 8), 'iD'],
-    "mouse": ['9', (5, 8), 'hD'],
-    "pencil": ['x', (3, 6), 'aD'],
-    "pp ball": ['y', (3, 5), 'gD']
+position = {
+    'upper': ('A', 'P'), 'lower': ('B', 'Q')
 }
+
+
+def getPosition(block):
+    if int(block[1:]) % 2 == 0:
+        pos = 'lower'
+    else:
+        pos = 'upper'
+    return position[pos]
 
 coordinates = {
     'D10': (1, 6), 'D11': (1, 5), 'D12': (1, 5), 'B12': (10, 6), 'B10': (10, 5),
@@ -24,12 +23,80 @@ coordinates = {
     'D2': (1, 10), 'D3': (1, 9), 'D1': (1, 10)
 }
 
-right = {
-    'A': (2, 2), 'B': (9, 2),
-    'C': (9, 9), 'D': (2, 9)
-}
 
-left = {
-    'A': (5, 2), 'B'(9, 5),
-    'C': (6, 9), 'D'(2, 6)
-}
+def getCoordinates(block):
+    return coordinates[block]
+
+
+class Objs(object):
+    def __init__(self, name, paw, goal, cart, grid):
+        self.name = name
+        self.paw = paw
+        self.goal = goal
+        self.cart = cart
+        self.grid = grid
+
+    def getName(self):
+        return self.name
+
+    def getPaw(self):
+        return self.paw
+
+    def getGoal(self):
+        return self.goal
+
+    def getCart(self):
+        return self.cart
+
+    def getGrid(self):
+        return self.grid
+
+
+yellow_cube = Objs('yellow cube', '2', (5, 3), '1', (5, 3))
+yakult = Objs('yakult', '3', (8, 5), '2', (7.5, 4.5))
+jdb = Objs('jdb', '4', (8, 5), '3', (7.5, 5))
+tennis_ball = Objs('tennis ball', '5', (8, 5), '4', (7.5, 5.5))
+mimi = Objs('mimi', '6', (6, 8), '5', (6.5, 7.5))
+wired_ball = Objs('wired ball', '7', (6, 8), '6', (6, 7.5))
+shuttercock = Objs('shuttercock', '8', (6, 8), '7', (5.5, 7.5))
+mouse = Objs('mouse', '9', (3, 6), '8', (3.5, 6.5))
+pencil = Objs('pencil', 'x', (3, 6), '9', (3.5, 6))
+pp_ball = Objs('pp ball', 'y', (3, 6), '10', (3.5, 5.5))
+
+
+def getObj(name):
+    objects = [yellow_cube, yakult, jdb, tennis_ball, mimi,
+               wired_ball, shuttercock, mouse, pencil, pp_ball]
+    for obj in objects:
+        if obj.getName() == name:
+            return obj
+
+
+class Shelf(object):
+    def __init__(self, name, left, right):
+        self.name = name
+        self.left = left
+        self.right = right
+
+    def getSide(self, side):
+        if side == 'left':
+            return self.left
+        if side == 'right':
+            return self.right
+        else:
+            print('Side input error!')
+
+    def getName(self):
+        return self.name
+
+A = Shelf('A', (5, 2), (2, 2))
+B = Shelf('B', (9, 5), (9, 2))
+C = Shelf('C', (6, 9), (9, 9))
+D = Shelf('D', (2, 6), (2, 9))
+
+
+def shelfSide(shelf, side):
+    shelfs = [A, B, C, D]
+    for slf in shelfs:
+        if slf.getName() == shelf:
+            return slf
