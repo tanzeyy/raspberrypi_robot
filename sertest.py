02b73bd1535fecb9ff3a159d9aa49bfa50ebde56
tanzeyy <tan_ze@outlook.com>
2017-04-17 18:34:56 +0800

    modified

--- sertest.py
+++ sertest.py
@@ -1,17 +1,15 @@
 import serial
-import json
 import classify
+import send_results as sr
 
 port = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 
 while True:
-    rcv = port.read(10)
+    rcv = port.read(7)
     print("Reciving:" + rcv)
     if rcv == "capture":
-        shelf = "A"
-        classify.run('A', 'right')
-        with open(('images_classify/results/%s' + '.json') % shelf) as results:
-            results = json.load(results)
-            for position, result in results.items():
-                msg = (position + ':' + result + ';').encode('ascii')
-                port.write(msg)
+        shelf = port.read(1)
+        side = port.read(5)
+        classify.run(shelf, side)
+        sr.sendResults(shelf)
+
