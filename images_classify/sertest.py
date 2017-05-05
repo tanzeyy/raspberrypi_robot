b9409bed03aa96b27baa3191ae56abf2654b4342
tanzeyy <tan_ze@outlook.com>
2017-05-05 09:27:36 +0800

    适应python3

--- images_classify/sertest.py
+++ images_classify/sertest.py
@@ -7,6 +7,10 @@ from capture import capture_images
 port = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 port.reset_input_buffer()
 
+send_data_end_char = '$'.encode('utf-8')
+classify_end_char = '~'.encode('utf-8')
+not_classify_end_char = '!'.encode('utf-8')
+
 
 def send_results():
     with open("results.txt", 'r+') as fh:
@@ -14,32 +18,33 @@ def send_results():
             result = fh.readline()
             if len(result) == 0:
                 break
-            port.write(result)
+            port.write(result.encode('utf-8'))
 
     fh.close()
     # Send the end character
-    port.write('$')
+    port.write(send_data_end_char)
 
 
 def run():
     while True:
-        rcv = port.read(1)
-        print("Reciving:" + rcv)
-        if rcv == "#":
-            shelf = port.read(1)
-            side = port.read(5)
+        rcv = port.read(1).decode('utf-8')
+        print('Reciving:' + rcv)
+        if rcv == '#':
+            rcv_data = port.read(6).decode('utf-8')
+            shelf = rcv_data[0]
+            side = rcv_data[1:].strip()
             print(shelf, side)
             capture_images(shelf, side)
             if shelf == 'A' and side == 'left':
                 image_classify(shelf)
-                port.write('~')
+                port.write(classify_end_char)
                 send_results()
             elif shelf == 'D' and side == 'left':
                 image_classify(shelf)
-                port.write('~')
+                port.write(classify_end_char)
                 send_results()
             else:
-                port.write('!')
+                port.write(not_classify_end_char)
 
 
 if __name__ == '__main__':
