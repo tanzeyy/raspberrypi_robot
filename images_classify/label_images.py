6d491a5b69a8896c951aa8aa30b47caeff92aff7
tanzeyy <tan_ze@outlook.com>
2017-05-09 00:11:14 +0800

    移动了tf_files

--- images_classify/label_images.py
+++ images_classify/label_images.py
@@ -9,20 +9,20 @@ import os
 
 
 def label_images():
-    images = os.listdir("images_classify/images/BCD")
+    images = os.listdir('images/BCD')
     results = {}
     final_results = {}
 
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line in
-                   tf.gfile.GFile("images_classify/tf_files/labels.txt")]
+                   tf.gfile.GFile('tf_files/labels.txt')]
 
     # Initial the results
     for line in label_lines:
         results[line] = ('no', 0)
 
     # Unpersists graph from file
-    with tf.gfile.FastGFile("images_classify/tf_files/graph.pb", 'rb') as f:
+    with tf.gfile.FastGFile('tf_files/graph.pb', 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
@@ -31,7 +31,7 @@ def label_images():
     with tf.Session() as sess:
         for image in images:
             softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-            image_data = tf.gfile.FastGFile(str("images_classify/images/BCD/" +
+            image_data = tf.gfile.FastGFile(str('images/BCD/' +
                                             image), 'rb').read()
             predictions = sess.run(softmax_tensor,
                                    {'DecodeJpeg/contents:0': image_data})
