6dcace3f68ad7a2c9f4a3a57dfa88931a1211870
tanzeyy <tan_ze@outlook.com>
2017-05-10 08:38:59 +0800

    update

--- images_classify/sertest.py
+++ images_classify/sertest.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 from classify import image_classify
+from capture import capture_images
 
 import cv2
 import numpy as np
@@ -13,9 +14,12 @@ import os
 port = serial.Serial('/dev/ttyUSB0', 9600)
 port.reset_input_buffer()
 
+# Define start and end character
 send_data_end_char = '$'
-# classify_end_char = '~'.encode('utf-8')
-# not_classify_end_char = '!'.encode('utf-8')
+capture_char = '#'
+capture_end_char = '!'
+classify_char = '~'
+capture_a_shelf_char = '@'
 
 if not os.path.exists('images/A'):
     os.makedirs('images/A')
@@ -37,6 +41,7 @@ def send_results():
     port.write(send_data_end_char)
 
 
+# Block number
 block = ['2', '1', '3', '4', '6', '5', '7', '8', '10', '9', '11', '12']
 
 
@@ -48,11 +53,12 @@ def run(cap_src):
         rcv = port.read(1)
         print('Recived: ' + rcv)
 
-        if rcv == '#':
+        if rcv == capture_char:
             cap = cv2.VideoCapture(cap_src)
             cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920)
             cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080)
             shelf = port.read(1)
+            print(shelf)
             if shelf == 'A':
                 folder = 'A'
             else:
@@ -75,11 +81,22 @@ def run(cap_src):
             num += 1
             if num == 12:
                 num = 0
-            port.write('!')
-        elif rcv == '~':
+            port.write(capture_end_char)
+
+        elif rcv == capture_a_shelf_char:
+            shelf = port.read(1)
+            print(shelf)
+            side = port.read(5).strip()
+            print(side)
+            capture_images(shelf, side)
+            port.write(capture_end_char)
+
+        elif rcv == classify_char:
             shelf = port.read(1)
             image_classify(shelf)
             send_results()
+
+        cap.release()
 
 
 if __name__ == '__main__':
